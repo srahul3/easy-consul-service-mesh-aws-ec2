8d4f4270a1a54c2589dd3569bd79e4a38c21a430
Rahul <srahul3@gmail.com>
2024-02-26 18:46:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,27 +2,11 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
+resource "aws_instance" "example_server" {
+  ami           = "ami-04e914639d0cca79a"
+  instance_type = "t2.micro"
 
   tags = {
-    Name = var.instance_name
+    Name = "TFCExample"
   }
 }
