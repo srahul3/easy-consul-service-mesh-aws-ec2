57fed30fb87a64beb3bcafb30814993f6ed6db40
Rahul <srahul3@gmail.com>
2024-02-26 18:39:12 +0530

    Adding terraform cloud support

---
+++ terraform.tf
@@ -0,0 +1,19 @@
+terraform {
+
+  cloud {
+    organization = "organization-name"
+
+    workspaces {
+      name = "learn-terraform-cloud"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.31.0"
+    }
+  }
+
+  required_version = "~> 1.2"
+}
